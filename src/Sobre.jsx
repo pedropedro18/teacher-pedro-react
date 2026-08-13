640181555798b9845b7d83d7fbb2851b8a3edc90
pedro <pt8066545@gmail.com>
2026-08-13 18:54:36 +0100

    atualizações do site

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -1,7 +1,7 @@
 export default function Sobre(){
     return(
 <section id="sobre" className="about">
-    <img src="public/imagens/IMG_20250529_070439_367.jpg" alt="Teacher Pedro" />
+    <img src="/imagens/IMG_20250529_070439_367.jpg" alt="Teacher Pedro" />
     <h2>Sobre mim</h2>
 <p>Professor de Inglês certificado, com experiência em todos os níveis do quadro europeu comum de referência (CEFR).</p>
 
