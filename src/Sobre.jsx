30852b311a62947dfcd4579019e2b8a8098fd36a
pedro <pt8066545@gmail.com>
2026-08-20 13:30:37 +0100

    Update Sobre section bio text

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -3,8 +3,8 @@ export default function Sobre(){
 <section id="sobre" className="about">
     <img src="/imagens/IMG_20250529_070439_367.jpg" alt="Teacher Pedro" />
     <h2>Sobre mim</h2>
-<p>Professor de inglês certificado, com experiência em todos os níveis do quadro europeu comum de referência (CEFR).</p>
-
+<p>
+Professor de inglês há 5 anos, formado pelo programa educador Maplebear. Ensino todos os níveis do (CEFR), dos níveis A1 a C2. Ministro também formação em excel e programação. Combino a experiência em sala de aula com competêcias em desenvolvimento web para criar recursos didáticos digitais acessíveis aos meus alunos. O que mais me motiva no ensino é trabalhar com crianças - a energia e a curiosidade delas tornam cada aula diferente, e é gratificante acompanhar o progresso de cada aluno ao longo do tempo..</p>
 </section>
 
     );
