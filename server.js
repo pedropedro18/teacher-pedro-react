ffcc388c7c51a7e1d0bc4bd8c4fc85ad32870312
pedro <pt8066545@gmail.com>
2026-08-27 16:50:35 +0100

    fix: Adiciona script start para rodar server.js no Railway

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { loginAluno, definirPasswordAluno, meuPerfil, verificarTokenAluno } from
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
