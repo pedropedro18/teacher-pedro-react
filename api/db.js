c9d4adedcd3866df29d9c4f9cfc9d98b4cf61a68
pedro <pt8066545@gmail.com>
2026-08-28 20:57:53 +0100

    fix: adiciona DB_PORT na conexão com o banco

--- api/db.js
+++ api/db.js
@@ -1,7 +1,8 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
