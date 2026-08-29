6a26619733307bf39d34efa4cf48e6fe613ac007
pedro <pt8066545@gmail.com>
2026-08-29 18:12:32 +0100

    Adiciona mais conteúdos ao nível A1

--- src/data/conteudos.js
+++ src/data/conteudos.js
@@ -4,11 +4,11 @@ export const conteudosPorNivel = {
       titulo: 'Saudações básicas',
       tipo: 'texto',
       conteudo: `Olá! - Hello!
-Bom dia! - Good morning!
-Boa tarde! - Good afternoon!
-Boa noite! - Good evening / Good night!
-Como estás? - How are you?
-Tudo bem, obrigado! - I'm fine, thank you!`
+      Bom dia! - Good morning!
+ Boa tarde! - Good afternoon!
+ Boa noite! - Good evening / Good night!
+ Como estás? - How are you?
+ Tudo bem, obrigado! - I'm fine, thank you!`
     },
     {
       titulo: 'Vocabulário do dia-a-dia',
@@ -18,7 +18,83 @@ Comida - Food
 Água - Water
 Trabalho - Work
 Amigo - Friend`
+},
+{
+      titulo: 'Números 0-20',
+      tipo: 'texto',
+      conteudo: `Zero - Zero
+Um - One
+Dois - Two
+Três - Three
+Quatro - Four
+Cinco - Five
+Seis - Six
+Sete - Seven
+Oito - Eight
+Nove - Nine
+Dez - Ten
+Onze - Eleven
+Doze - Twelve
+Treze - Thirteen
+Catorze - Fourteen
+Quinze - Fifteen
+Dezasseis - Sixteen
+Dezassete - Seventeen
+Dezoito - Eighteen
+Dezanove - Nineteen
+Vinte - Twenty`
     },
+    {
+      titulo: 'Cores',
+      tipo: 'texto',
+      conteudo: `Vermelho - Red
+Azul - Blue
+Verde - Green
+Amarelo - Yellow
+Preto - Black
+Branco - White
+Cor de rosa - Pink
+Roxo - Purple
+Laranja - Orange
+Castanho - Brown`
+    },
+    {
+      titulo: 'Dias da semana',
+      tipo: 'texto',
+      conteudo: `Segunda-feira - Monday
+Terça-feira - Tuesday
+Quarta-feira - Wednesday
+Quinta-feira - Thursday
+Sexta-feira - Friday
+Sábado - Saturday
+Domingo - Sunday`
+    },
+    {
+      titulo: 'Família',
+      tipo: 'texto',
+      conteudo: `Mãe - Mother
+Pai - Father
+Irmão - Brother
+Irmã - Sister
+Avô - Grandfather
+Avó - Grandmother
+Filho - Son
+Filha - Daughter
+Tio - Uncle
+Tia - Aunt`
+    },
+    {
+      titulo: 'Verbo To Be (presente)',
+      tipo: 'texto',
+      conteudo: `I am - Eu sou/estou
+You are - Tu és/estás
+He is - Ele é/está
+She is - Ela é/está
+It is - Isto é/está
+We are - Nós somos/estamos
+They are - Eles são/estão`
+    },
+  
   ],
   A2: [
     { titulo: 'Passado simples', link: 'https://...', tipo: 'vídeo' },
