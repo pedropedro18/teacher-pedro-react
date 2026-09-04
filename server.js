db3588e43bbe93df65b154167ad7151eb3ec19de
pedro <pt8066545@gmail.com>
2026-09-04 11:41:41 +0100

    Corrige require para import em server.js (ES modules)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import certificadoRouter from './api/certificado.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import materiaisRoutes from './api/materiais';
+import materiaisRoutes from './api/materiais';git
 
 
 const app = express();
